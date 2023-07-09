9754859e8fcbe403787461aecab2b8c29638526a
youz2me <kynhun20@gachon.ac.kr>
2023-07-09 20:53:26 +0900

    Feat: LoginViewController 초기화
    브랜치 분리(developer, feature/login-ui) 및 LoginViewController 파일 생성함. Alamofire와 Snapkit import함.
    
    #1

---
+++ Bookjam/View/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/07/09.
+//
+
+import UIKit
+
+import Alamofire
+import SnapKit
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
