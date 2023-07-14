8ac702ceed1c9a3b17e0ae7fa20178a0b3245037
youz2me <kynhun20@gachon.ac.kr>
2023-07-15 01:10:40 +0900

    Feat: Onboarding07VC 초기화, 기본 템플릿 ViewController에 적용
    #7

---
+++ Bookjam/View/Onboarding/Onboarding07VC.swift
@@ -0,0 +1,56 @@
+//
+//  Onboarding07VC.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/07/15.
+//
+
+import SwiftUI
+import UIKit
+
+import Alamofire
+import SnapKit
+import Then
+
+
+class Onboarding07VC: UIViewController {
+    
+    // MARK: Variables
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    
+    func setUpView() {
+        
+    }
+    
+    
+    // MARK: Layout
+    
+    func setUpLayout() {
+        
+    }
+    
+    
+    // MARK: Constraint
+    
+    func setUpConstraint() {
+        
+    }
+    
+}
+
+struct Onboarding07VC_Preview: PreviewProvider {
+    static var previews: some View {
+        Onboarding07VC().toPreview()
+            // .edgesIgnoringSafeArea(.all)
+    }
+}
