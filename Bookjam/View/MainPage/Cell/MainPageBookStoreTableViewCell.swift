bc21e228fbefffa02a1e3c343e668da7d48c3147
jangjoonmo <jjmo97@ajou.ac.kr>
2023-08-04 04:44:23 +0900

    Feat: MainPageBookStoreTableView 변수 제약조건들 추가

--- Bookjam/View/MainPage/Cell/MainPageBookStoreTableViewCell.swift
+++ Bookjam/View/MainPage/Cell/MainPageBookStoreTableViewCell.swift
@@ -40,6 +40,49 @@ class MainPageBookStoreTableViewCell: UITableViewCell {
     
     // MARK: Variable
 
+    var bookstoreLabel: UILabel = UILabel().then {
+        $0.font = title06
+        $0.textColor = .black
+        $0.text = "책발전소광교"
+        $0.sizeToFit()
+    }
+    
+    var bookMarkImageView: UIImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "book.fill")
+        $0.tintColor = gray04
+    }
+    
+    var timeButton: UIButton = UIButton().then{
+        $0.setTitle("영업중", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 10
+        $0.backgroundColor = UIColor(hexCode: "#00C950")
+    }
+    var starImageView: UIImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "star.fill")
+        $0.tintColor = warning
+    }
+    
+    var starLabel: UILabel = UILabel().then {
+        $0.font = paragraph03
+        $0.text = "4.53"
+    }
+    
+    var reviewCountLabel: UILabel = UILabel().then {
+        $0.font = paragraph03
+        $0.text = "리뷰 132"
+        $0.sizeToFit()
+    }
+    
+    var underLineView: UIView = UIView().then {
+        $0.backgroundColor = gray04
+    }
+    
+    var locationLabel: UILabel = UILabel().then {
+        $0.font = paragraph05
+        $0.textColor = gray06
+        $0.text = "경기도 수원시 팔달구 매산로52번길 20"
+    }
     
     // MARK: View
     
@@ -53,17 +96,52 @@ class MainPageBookStoreTableViewCell: UITableViewCell {
     
     func setUpLayout() {
         [
-           
+           bookstoreLabel,
+           bookMarkImageView,
+           timeButton,
+           starImageView,
+           starLabel,
+           reviewCountLabel,
+           locationLabel,
+           timeButton
         ].forEach { self.addSubview($0) }
     }
     
     // MARK: Constraint
     
     func setUpConstraint() {
-        
+        bookstoreLabel.snp.makeConstraints{
+            $0.leading.equalToSuperview().offset(10)
+        }
+        bookMarkImageView.snp.makeConstraints{
+            $0.centerY.equalTo(bookstoreLabel)
+            $0.leading.equalTo(bookstoreLabel.snp.trailing).offset(10)
+        }
+        locationLabel.snp.makeConstraints{
+            $0.top.equalTo(bookstoreLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(bookstoreLabel.snp.leading)
+        }
+        timeButton.snp.makeConstraints{
+            $0.top.equalTo(locationLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(bookstoreLabel.snp.leading)
+        }
+        starImageView.snp.makeConstraints{
+            $0.leading.equalTo(timeButton.snp.trailing).offset(15)
+            $0.centerY.equalTo(timeButton.snp.centerY)
+            
+        }
+        starLabel.snp.makeConstraints{
+            $0.leading.equalTo(starImageView.snp.trailing).offset(5)
+            $0.centerY.equalTo(timeButton.snp.centerY)
+
+        }
+        reviewCountLabel.snp.makeConstraints{
+            $0.leading.equalTo(starLabel.snp.trailing).offset(10)
+            $0.centerY.equalTo(timeButton.snp.centerY)
+        }
     }
 
-}
+}//end of MainPageBookStoreTableViewCell
 
 #if DEBUG
 import SwiftUI
