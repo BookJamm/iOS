1e2560733cb44fa0c750080481c5938837783e79
youz2me <kynhun20@gachon.ac.kr>
2023-08-04 03:51:47 +0900

    Feat: 참여 탭 구현 완료

---
+++ Bookjam/Model/ActivityInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityInfo.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/08/03.
+//
+
+import Foundation
+
+struct Activity {
+    var photo: String
+    var name: String
+    var starValue: Float
+    var numOfReview: Int
+    var description: String
+}
