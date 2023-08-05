760f1cb59070fc2d70ea286ff6711731538db9bc
jangjoonmo <jjmo97@ajou.ac.kr>
2023-08-06 02:08:31 +0900

    chore: Pull 받기 위해 변경사항 저장

---
+++ Bookjam/Screen/BookStoreDetailBookListView.swift
@@ -0,0 +1,60 @@
+//
+//  BookStoreDetailBookTypeView.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/07/30.
+//
+
+import SwiftUI
+import UIKit
+
+import SnapKit
+import Then
+
+class BookStoreDetailBookListView: UIViewController {
+
+    // MARK: Variables
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    
+    func setUpView() {
+        
+    }
+    
+    
+    // MARK: Layout
+    
+    func setUpLayout() {
+        
+    }
+    
+    // MARK: Delegate
+    
+    func setUpDelegate() {
+        
+    }
+    
+    
+    // MARK: Constraint
+    
+    func setUpConstraint() {
+        
+    }
+
+}
+
+struct BookStoreDetailBookListView_Preview: PreviewProvider {
+    static var previews: some View {
+        BookStoreDetailBookListView().toPreview()
+            // .edgesIgnoringSafeArea(.all)
+    }
+}
