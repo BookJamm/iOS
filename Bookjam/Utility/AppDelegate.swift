8005f420cecf34ce63ad9ade199aa8b7e7ee86fd
youz2me <kynhun20@gachon.ac.kr>
2023-08-07 17:26:08 +0900

    Refactor: Extension 분리
    #28

--- Bookjam/Utility/AppDelegate.swift
+++ Bookjam/Utility/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by YOUJIM on 2023/07/08.
 //
 
+import SwiftUI
 import UIKit
 
 @main
@@ -29,160 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-}
-
-extension UIViewController {
-    func hideKeyboard() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-}
-
-extension UIScrollView {
-    func updateContentSize() {
-        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
         
-        // 계산된 크기로 컨텐츠 사이즈 설정
-        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
-    }
-    
-    private func recursiveUnionInDepthFor(view: UIView) -> CGRect {
-        var totalRect: CGRect = .zero
-        
-        // 모든 자식 View의 컨트롤의 크기를 재귀적으로 호출하며 최종 영역의 크기를 설정
-        for subView in view.subviews {
-            totalRect = totalRect.union(recursiveUnionInDepthFor(view: subView))
-        }
-        
-        // 최종 계산 영역의 크기를 반환
-        return totalRect.union(view.frame)
-    }
-}
-
-#if DEBUG
-extension UIViewController {
-    private struct Preview: UIViewControllerRepresentable {
-            let viewController: UIViewController
-
-            func makeUIViewController(context: Context) -> UIViewController {
-                return viewController
-            }
-
-            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-            }
-        }
-
-        func toPreview() -> some View {
-            Preview(viewController: self)
-        }
-}
-#endif
-
-extension UIColor {
-    
-    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if hexFormatted.hasPrefix("#") {
-            hexFormatted = String(hexFormatted.dropFirst())
-        }
-        
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-        
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-        
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
-}
-
-#if canImport(SwiftUI) && DEBUG
-    import SwiftUI
-
-    public struct UIViewPreview<View: UIView>: UIViewRepresentable {
-        public let view: View
-        public init(_ builder: @escaping () -> View) {
-            view = builder()
-        }
-        // MARK: - UIViewRepresentable
-        public func makeUIView(context: Context) -> UIView {
-            return view
-        }
-        public func updateUIView(_ view: UIView, context: Context) {
-            view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-            view.setContentHuggingPriority(.defaultHigh, for: .vertical)
-        }
-    }
-
-    public struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
-        public let viewController: ViewController
-
-        public init(_ builder: @escaping () -> ViewController) {
-            viewController = builder()
-        }
-
-        // MARK: - UIViewControllerRepresentable
-        public func makeUIViewController(context: Context) -> ViewController {
-            viewController
-        }
-
-        @available(iOS 13.0, tvOS 13.0, *)
-        @available(OSX, unavailable)
-        @available(watchOS, unavailable)
-        public func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {
-            return
-        }
-    }
-#endif
-
-extension UIImage {
-    func circularImage() -> UIImage? {
-        let shorterSide = UIImage(named: "BasicProfile")?.size.width
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: shorterSide!, height: shorterSide!))
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = shorterSide! / 2.0
-        imageView.layer.masksToBounds = true
-        imageView.image = self
-
-        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return nil
-        }
-        imageView.layer.render(in: context)
-        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return roundedImage
-    }
-}
-
-extension UIView {
-   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        let mask = CAShapeLayer()
-        mask.path = path.cgPath
-        layer.mask = mask
     }
 }
