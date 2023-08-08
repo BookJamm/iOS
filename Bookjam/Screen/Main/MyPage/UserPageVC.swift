7af28f0a0a5b40d589914cafde10be99dfbd7a56
youz2me <kynhun20@gachon.ac.kr>
2023-08-08 10:43:25 +0900

    Build: 브랜치 생성 및 UserPageVC 초기화

---
+++ Bookjam/Screen/Main/MyPage/UserPageVC.swift
@@ -0,0 +1,63 @@
+//
+//  UserPageVC.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/08/08.
+//
+
+// MARK: - 마이페이지에서 프로필 수정하기 버튼을 눌렀을 때 넘어오는 유저 페이지 화면
+
+import SwiftUI
+import UIKit
+
+import SnapKit
+import Then
+
+
+class UserPageVC: UIViewController {
+
+    // MARK: Variables
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    
+    func setUpView() {
+        
+    }
+    
+    
+    // MARK: Layout
+    
+    func setUpLayout() {
+        
+    }
+    
+    // MARK: Delegate
+    
+    func setUpDelegate() {
+        
+    }
+    
+    
+    // MARK: Constraint
+    
+    func setUpConstraint() {
+        
+    }
+    
+}
+
+struct UserPageVC_Preview: PreviewProvider {
+    static var previews: some View {
+        UserPageVC().toPreview()
+            // .edgesIgnoringSafeArea(.all)
+    }
+}
