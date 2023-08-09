b108ad8cb1b7b57d31faf278abb3dde9ccc64da5
youz2me <kynhun20@gachon.ac.kr>
2023-08-09 16:53:48 +0900

    Feat: 이메일 중복 확인 API 구현 완료

---
+++ Bookjam/API/APIConstant.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstant.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/08/09.
+//
+
+import Foundation
+
+
+struct Constant {
+    static let baseURL = "https://bookjam.shop"
+    
+    static let postEmailDuplicate = "/auth/email-check"
+}
--- Bookjam/API/APIManager.swift
+++ Bookjam/API/APIManager.swift
@@ -5,36 +5,16 @@
 //  Created by YOUJIM on 2023/08/09.
 //
 
+// MARK: 서버와의 API 통신을 위한 APIManager 구현
+
 import Foundation
 
 import Alamofire
 
 
-private let BASE_URL = "http://Bookjam-dev-env.eba-nshzkmkh.ap-northeast-2.elasticbeanstalk.com"
-
 class APIManager: ObservableObject  {
     static let shared = APIManager()
-    
-    private var appleId: String = ""
     private var headers: HTTPHeaders?
-    
-    private func setHeader() {
-        self.headers = ["apple-code": appleId]
-    }
-    
-    public func getAppleId() -> String {
-        // TODO: UserDefaults에 저장한 유저 식별값 반환
-        return appleId
-    }
-    
-    public func setAppleId() {
-        if UserDefaults.standard.value(forKey: "appleId") != nil {
-            if let appleId = UserDefaults.standard.object(forKey: "appleId") as? String {
-                self.appleId = appleId
-                setHeader()
-            }
-        }
-    }
 }
 
 // MARK: T에는 요청값 데이터의 모델, U에는 응닶값 데이터의 모델 적기
@@ -45,7 +25,7 @@ extension APIManager {
                                            parameter: T?,
                                            completionHandler: @escaping (U)->Void) {
         
-        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        guard let url = URL(string: Constant.baseURL + urlEndpointString) else { return }
         print("get 요청 URL --> \(url)")
 
         AF
@@ -67,7 +47,7 @@ extension APIManager {
                                             requestDataType: T.Type,
                                             parameter: T?,
                                             completionHandler: @escaping (U)->Void) {
-        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        guard let url = URL(string: Constant.baseURL + urlEndpointString) else { return }
         print("post 요청 URL --> \(url)")
 
         AF
@@ -90,10 +70,32 @@ extension APIManager {
                                             parameter: T?,
                                             completionHandler: @escaping (U)->Void) {
         
-        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        guard let url = URL(string: Constant.baseURL + urlEndpointString) else { return }
         print("patch 요청 URL --> \(url)")
         AF
             .request(url, method: .patch, parameters: parameter, encoder: .json, headers: self.headers)
+            .responseDecodable(of: U.self) { response in
+                print(response)
+                switch response.result {
+                case .success(let success):
+                    completionHandler(success)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            .resume()
+    }
+    
+    func deleteData<T: Codable, U: Decodable>(urlEndpointString: String,
+                                            responseDataType: U.Type,
+                                            requestDataType: T.Type,
+                                            parameter: T?,
+                                            completionHandler: @escaping (U)->Void) {
+        
+        guard let url = URL(string: Constant.baseURL + urlEndpointString) else { return }
+        print("patch 요청 URL --> \(url)")
+        AF
+            .request(url, method: .delete, parameters: parameter, encoder: .json, headers: self.headers)
             .responseDecodable(of: U.self) { response in
                 print(response)
                 switch response.result {
