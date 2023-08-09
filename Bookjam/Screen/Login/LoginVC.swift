d3fedbcce1d4b297b24f0e825a0f05ad440a6041
youz2me <kynhun20@gachon.ac.kr>
2023-08-09 21:21:10 +0900

    Feat: 브랜치 분리 및 LoginVC 생성

---
+++ Bookjam/Screen/Login/LoginVC.swift
@@ -0,0 +1,62 @@
+//
+//  LoginVC.swift
+//  Bookjam
+//
+//  Created by YOUJIM on 2023/08/09.
+//
+
+import SwiftUI
+import UIKit
+
+import SnapKit
+import Then
+
+
+class LoginVC: UIViewController {
+
+    // MARK: Variables
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    
+    func setUpView() {
+        
+    }
+    
+    
+    // MARK: Layout
+    
+    func setUpLayout() {
+        
+    }
+    
+    // MARK: Delegate
+    
+    func setUpDelegate() {
+        
+    }
+    
+    
+    // MARK: Constraint
+    
+    func setUpConstraint() {
+        
+    }
+    
+}
+
+struct LoginVC_Preview: PreviewProvider {
+    static var previews: some View {
+        LoginVC().toPreview()
+            // .edgesIgnoringSafeArea(.all)
+    }
+}
+
