2e0d5f5284f1957c1997e3642dbe9bf21bfacab0
youz2me <kynhun20@gachon.ac.kr>
2023-08-13 12:06:33 +0900

    Feat: 로그인 API 연결 및 화면 전환 구현

--- Bookjam/Resource/Model/UserInfo.swift
+++ Bookjam/Resource/Model/UserInfo.swift
@@ -5,37 +5,44 @@
 //  Created by 장준모 on 2023/07/21.
 //
 
-// MARK: - 
+// MARK: - 유저 정보 저장을 위한 싱글톤 클래스 선언
 
 import Foundation
-import Alamofire
 
-struct User: Codable {
-   
-    var user_id: Int
-    var name: String
-    var email: String
-    var password: String
-    var mobile: String
-    var profile_image: String?
-    var username: String
-    var created_at: String
-    var updated_at: String?
-    var disabled_at: String?
-    var kakao_token: String?    //카카오 토큰인데 임시로 타입을 String으로 하였습니다.
+/// 싱글톤 클래스 선언
+class UserInfo {
+    static let shared = UserInfo()
     
-    enum CodingKeys: String, CodingKey {
-        case user_id = "user_id"
-        case name = "name"
-        case email = "email"
-        case password = "password"
-        case mobile = "mobile"
-        case profile_image = "profile_image"
-        case username = "username"
-        case created_at = "created_at"
-        case updated_at = "updated_at"
-        case disabled_at = "disabled_at"
-        case kakao_token = "kakao_token"
-    }
+    var user = User()
     
+    private init() {}
+}
+
+struct User {
+
+    var ID: Int?
+    var name: String?
+    var email: String?
+    var password: String?
+    // var mobile: String?
+    // var profileImage: String?
+    // var username: String?
+    // var createdAt: String?
+    // var updatedAt: String?
+    // var disabledAt: String?
+    var kakaoToken: String?    //카카오 토큰인데 임시로 타입을 String으로 하였습니다.
+
+//    enum CodingKeys: String, CodingKey {
+//        case ID = "user_id"
+//        case name = "name"
+//        case email = "email"
+//        case password = "password"
+//        case mobile = "mobile"
+//        case profileImage = "profile_image"
+//        case username = "username"
+//        case createdAt = "created_at"
+//        case updatedAt = "updated_at"
+//        case disabledAt = "disabled_at"
+//        case kakaoToken = "kakao_token"
+//    }
 }
