e238c22a611b734c81b7a902e3f76cdd40c55762
youz2me <kynhun20@gachon.ac.kr>
2023-08-15 14:20:51 +0900

    Refactor: 이상한 곳으로 가있는 폴더 재위치

--- Bookjam/API/APIConstant.swift
+++ Bookjam/API/APIConstant.swift
@@ -10,9 +10,7 @@ import Foundation
 
 struct Constant {
     static let baseURL = "https://bookjam.shop"
-    
     static let postEmailDuplicate = "/auth/email-check"
     static let searchFriend = "/user/search"
     static let userLogin = "/auth/login"
-    
 }
