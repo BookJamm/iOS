f26bb532a589c7bbc19be03a8b06a3445ebec281
youz2me <kynhun20@gachon.ac.kr>
2023-08-22 03:34:57 +0900

    Fix: 비밀번호 설정 조건에 안맞아도 다음 페이지로 넘어가던 오류 수정

--- Bookjam/Screen/Onboarding/Onboarding03VC.swift
+++ Bookjam/Screen/Onboarding/Onboarding03VC.swift
@@ -87,6 +87,7 @@ class Onboarding03VC: UIViewController {
         $0.layer.cornerRadius = 8
         $0.layer.masksToBounds = true
         $0.backgroundColor = gray04
+        $0.isEnabled = false
         $0.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         $0.addTarget(self, action: #selector(didNextButtonTapped), for: .touchDown)
     }
