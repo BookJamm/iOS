1f2da587b6e0510e3dfd6e3366e910da771df379
FpRaArNkK <mspark1184@naver.com>
2023-09-07 02:59:15 +0900

    Feat : filter 모델 추가

---
+++ Bookjam/Screen/Main/LocationPage/Model/filterModel.swift
@@ -0,0 +1,13 @@
+//
+//  filterEnum.swift
+//  Bookjam
+//
+//  Created by 박민서 on 2023/09/07.
+//
+
+/// 검색 필터 모델입니다.
+enum filters: String {
+    case rating = "rating"
+    case review = "review"
+    case distance = "distance"
+}
