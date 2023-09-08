f75fa4ffab03243bca5918a1e91e8666234ebeb3
youz2me <kynhun20@gachon.ac.kr>
2023-09-08 14:37:44 +0900

    Fix: 프리뷰 주석 처리

--- Bookjam/Screen/Main/MainPage/MainPageVC.swift
+++ Bookjam/Screen/Main/MainPage/MainPageVC.swift
@@ -434,9 +434,9 @@ extension MainPageVC: CLLocationManagerDelegate{
     }
 }
 
-struct MainPageVC_Preview: PreviewProvider {
-    static var previews: some View {
-        MainPageVC().toPreview()
-            //.edgesIgnoringSafeArea(.all)
-    }
-}
+//struct MainPageVC_Preview: PreviewProvider {
+//    static var previews: some View {
+//        MainPageVC().toPreview()
+//            //.edgesIgnoringSafeArea(.all)
+//    }
+//}
