63392c0d64d73c193b03f2d0d92a3f4673f4a57c
jangjoonmo <jjmo97@ajou.ac.kr>
2023-11-15 19:19:23 +0900

    feat: Network 폴더 생성

---
+++ BookJam/Source/DetailPage/Model/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  BookJam
+//
+//  Created by 장준모 on 11/15/23.
+//
+
+import Foundation
---
+++ BookJam/Source/DetailPage/ViewModel/MainDetailPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainDetailPageViewModel.swift
+//  BookJam
+//
+//  Created by 장준모 on 11/15/23.
+//
+
+import Foundation
