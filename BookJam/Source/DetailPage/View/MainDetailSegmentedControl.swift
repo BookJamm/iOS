b50785456c3bb39ec2035ca3a0c8846506b1c52f
jangjoonmo <jjmo97@ajou.ac.kr>
2023-11-16 17:49:14 +0900

    feat: segmentedControl 추가

---
+++ BookJam/Source/DetailPage/View/MainDetailSegmentedControl.swift
@@ -0,0 +1,20 @@
+//
+//  MainDetailSegmentedControl.swift
+//  BookJam
+//
+//  Created by 장준모 on 11/16/23.
+//
+
+import UIKit
+
+class MainDetailSegmentedControl: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
