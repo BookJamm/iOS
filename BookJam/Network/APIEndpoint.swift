d12f6e827fc8a48a720cdd66e51a36316c0defb3
FpRaArNkK <mspark1184@naver.com>
2023-11-28 18:24:35 +0900

    [Feat] APIManager에서 Header 분리 -> APIEndPoint에 추가 + APIManager Rx 추가 적용 (#91)

--- BookJam/Network/APIEndpoint.swift
+++ BookJam/Network/APIEndpoint.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 enum NetworkError: Error {
     case invalidURL
@@ -18,6 +19,39 @@ enum NetworkError: Error {
 //enum HTTPMethod: String {
 //
 //}
+
+enum APIHeader {
+    // 얘네 테스트용이니 다 날려야 함
+    static let jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzLCJpYXQiOjE2OTIwMDExNDQsImV4cCI6MTc5MjAwNDc0NCwiaXNzIjoiYm9va2phbSJ9.BGN1CGC4Zu8xRVuH_zA-aTK1GYMWvNitR2mAfgG1zWU"
+
+    static let kakaoAppID = "e71c9521872b70f64acf3a7139889342"
+    
+    case header
+    case sendImageHeader
+    
+    var getValue: HTTPHeaders {
+        switch self {
+        case .header :
+            return [    //임시 슈퍼 jwt 토큰
+                "Authorization": "Bearer \(APIHeader.jwtToken)"
+            ]
+        case .sendImageHeader :
+            return [
+                "Authorization": "Bearer \(APIHeader.jwtToken)",
+                "Content-Type" : "multipart/form-data"
+            ]
+        }
+    }
+
+    // 추후 Token 로직 구현되었을 때 예시 with KeyChain
+//    case .logout:
+//        return ["Authorization": KeyChain.shared.read(account: .accessToken),
+//                "Refresh-Token": KeyChain.shared.read(account: .refreshToken),
+//                "Content-Type": "application/json"]
+//    default:
+//        return ["Authorization": KeyChain.shared.read(account: .accessToken),
+//                "Content-Type": "application/json"]
+}
 
 
 enum APIEndPoint {
--- BookJam/Network/APIManager.swift
+++ BookJam/Network/APIManager.swift
@@ -13,26 +13,11 @@ import RxSwift
 
 class APIManager: ObservableObject  {
     static let shared = APIManager()
-    private lazy var headers: HTTPHeaders = [    //임시 슈퍼 jwt 토큰
-        "Authorization": "Bearer \(jwtToken)"
-    ]
-    
-    func setHeaderToSendImage() -> HTTPHeaders {
-        headers = [
-            "Authorization": "Bearer \(jwtToken)",
-            "Content-Type" : "multipart/form-data"
-        ]
-        return headers
-    }
-    
-    // 얘네 테스트용이니 다 날려야 함
-    let jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzLCJpYXQiOjE2OTIwMDExNDQsImV4cCI6MTc5MjAwNDc0NCwiaXNzIjoiYm9va2phbSJ9.BGN1CGC4Zu8xRVuH_zA-aTK1GYMWvNitR2mAfgG1zWU"
-
-    let kakaoAppID = "e71c9521872b70f64acf3a7139889342"
 }
 
 // MARK: T에는 요청값 데이터의 모델, U에는 응닶값 데이터의 모델 적기
 extension APIManager {
+    
     func getData<T: Encodable, U: Decodable>(urlEndpointString: APIEndPoint,
                                            responseDataType: U.Type,
                                            requestDataType: T.Type,
@@ -42,13 +27,14 @@ extension APIManager {
         guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else {
             return Observable.error(NetworkError.invalidURL)
         }
+        
         print("get 요청 URL --> \(url)")
         print("Request 쿼리 --> \(String(describing: parameter))")
         
         
         return Observable.create { emitter in
             AF
-                .request(url, method: .get, parameters: parameter, headers: self.headers)
+                .request(url, method: .get, parameters: parameter, headers: APIHeader.header.getValue)
 //                .validate(statusCode: 200..<300) // validation code를 몰라요
                 .responseDecodable(of: BaseResponse<U>.self) { response in
                     print(response)
@@ -72,24 +58,35 @@ extension APIManager {
     func postData<T: Codable, U: Decodable>(urlEndpointString: APIEndPoint,
                                             responseDataType: U.Type,
                                             requestDataType: T.Type,
-                                            parameter: T?,
-                                            completionHandler: @escaping (U)->Void) {
-        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else { return }
+                                            parameter: T?
+    ) -> Observable<U>{
+        
+        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else {
+            return Observable.error(NetworkError.invalidURL)
+        }
+        
         print("post 요청 URL --> \(url)")
-        print("Request 쿼리 --> \(parameter)")
-
-        AF
-            .request(url, method: .post, parameters: parameter, encoder: .json, headers: self.headers)
-            .responseDecodable(of: U.self) { response in
-                //print(response)
-                switch response.result {
-                case .success(let success):
-                    completionHandler(success)
-                case .failure(let error):
-                    print(error.localizedDescription)
+        print("Request 쿼리 --> \(String(describing: parameter))")
+        
+        return Observable.create { emitter in
+            AF
+                .request(url, method: .post, parameters: parameter, encoder: .json, headers: APIHeader.header.getValue)
+                .responseDecodable(of: BaseResponse<U>.self) { response in
+                    //print(response)
+                    switch response.result {
+                    case .success(let success):
+                        if let result = success.result {
+                            emitter.onNext(result)
+                        }
+                        else {
+                            emitter.onError(NetworkError.invalidResponse)
+                        }
+                    case .failure(let error):
+                        emitter.onError(error)
+                    }
                 }
-            }
-            .resume()
+            return Disposables.create()
+        }
     }
     
     /// 서버에 이미지 post할 때 사용합니다.
@@ -97,76 +94,111 @@ extension APIManager {
     func postImage<U: Decodable>(urlEndpointString: APIEndPoint,
                                             responseDataType: U.Type,
                                             images: [Data],
-                                            completionHandler: @escaping (U)->Void) {
-        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else { return }
+                                            completionHandler: @escaping (U)->Void
+    ) -> Observable<U> {
+        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else {
+            return Observable.error(NetworkError.invalidURL)
+        }
+        
         print("post image 요청 URL --> \(url)")
         
-        AF.upload(multipartFormData: { MultipartFormData in
-            for image in images {
-                MultipartFormData.append(image, withName: "images", fileName: "\(image).png", mimeType: "image/png")
+        return Observable.create { emitter in
+        
+            AF.upload(multipartFormData: { MultipartFormData in
+                for image in images {
+                    MultipartFormData.append(image, withName: "images", fileName: "\(image).png", mimeType: "image/png")
+                }
+            }, to: url, method: .post, headers: APIHeader.sendImageHeader.getValue)
+            .validate() // 어떤 값을 validate 하는 지 확인 필요
+            .responseDecodable(of: BaseResponse<U>.self) { response in
+//                print(response)
+                switch response.result {
+                case .success(let success):
+                    if let result = success.result {
+                        emitter.onNext(result)
+                    }
+                    else {
+                        emitter.onError(NetworkError.invalidResponse)
+                    }
+                case .failure(let error):
+                    emitter.onError(error)
+                }
             }
-        }, to: url, method: .post, headers: setHeaderToSendImage())
-        .validate()
-        .responseDecodable(of: U.self) { response in
-            print(response)
-            switch response.result {
-            case .success(let success):
-                completionHandler(success)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
+//            .responseString(completionHandler: { response in
+//                print(response)
+//            }) // 이건 response 디버그용?
+//            .resume()
+            return Disposables.create()
         }
-        .responseString(completionHandler: { response in
-            print(response)
-        })
-        .resume()
     }
     
     func patchData<T: Codable, U: Decodable>(urlEndpointString: APIEndPoint,
                                             responseDataType: U.Type,
                                             requestDataType: T.Type,
-                                            parameter: T?,
-                                            completionHandler: @escaping (U)->Void) {
+                                            parameter: T?
+    ) -> Observable<U> {
         
-        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else { return }
+        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else {
+            return Observable.error(NetworkError.invalidURL)
+        }
+        
         print("patch 요청 URL --> \(url)")
-        print("Request 쿼리 --> \(parameter)")
+        print("Request 쿼리 --> \(String(describing: parameter))")
         
-        AF
-            .request(url, method: .patch, parameters: parameter, encoder: .json, headers: self.headers)
-            .responseDecodable(of: U.self) { response in
-                //print(response)
-                switch response.result {
-                case .success(let success):
-                    completionHandler(success)
-                case .failure(let error):
-                    print(error.localizedDescription)
+        return Observable.create { emitter in
+            AF
+                .request(url, method: .patch, parameters: parameter, encoder: .json, headers: APIHeader.header.getValue)
+                .responseDecodable(of: BaseResponse<U>.self) { response in
+                    //print(response)
+                    switch response.result {
+                    case .success(let success):
+                        if let result = success.result {
+                            emitter.onNext(result)
+                        }
+                        else {
+                            emitter.onError(NetworkError.invalidResponse)
+                        }
+                    case .failure(let error):
+                        emitter.onError(error)
+                    }
                 }
-            }
-            .resume()
+//                .resume()
+            return Disposables.create()
+        }
     }
     
     func deleteData<T: Codable, U: Decodable>(urlEndpointString: APIEndPoint,
                                             responseDataType: U.Type,
                                             requestDataType: T.Type,
-                                            parameter: T?,
-                                            completionHandler: @escaping (U)->Void) {
+                                            parameter: T?
+    ) -> Observable<U> {
         
-        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else { return }
+        guard let url = URL(string: APIEndPoint.baseURL + urlEndpointString.url) else {
+            return Observable.error(NetworkError.invalidURL)
+        }
+        
         print("patch 요청 URL --> \(url)")
-        print("Request 쿼리 --> \(parameter)")
+        print("Request 쿼리 --> \(String(describing: parameter))")
         
-        AF
-            .request(url, method: .delete, parameters: parameter, encoder: .json, headers: self.headers)
-            .responseDecodable(of: U.self) { response in
-                //print(response)
-                switch response.result {
-                case .success(let success):
-                    completionHandler(success)
-                case .failure(let error):
-                    print(error.localizedDescription)
+        return Observable.create { emitter in
+            AF
+                .request(url, method: .delete, parameters: parameter, encoder: .json, headers: APIHeader.sendImageHeader.getValue)
+                .responseDecodable(of: BaseResponse<U>.self) { response in
+                    //print(response)
+                    switch response.result {
+                    case .success(let success):
+                        if let result = success.result {
+                            emitter.onNext(result)
+                        }
+                        else {
+                            emitter.onError(NetworkError.invalidResponse)
+                        }
+                    case .failure(let error):
+                        emitter.onError(error)
+                    }
                 }
-            }
-            .resume()
+//                .resume()
+            return Disposables.create()
+        }
     }
 }
