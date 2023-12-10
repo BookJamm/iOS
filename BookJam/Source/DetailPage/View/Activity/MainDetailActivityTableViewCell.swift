b591e3ba7408997fa4ca042ad5110226d864e8c2
Jangmo <jjmo97@naver.com>
2023-12-10 21:31:00 +0900

    feat: 활동 테이블뷰 셀 생성

---
+++ BookJam/Source/DetailPage/View/Activity/MainDetailActivityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainDetailActivityTableViewCell.swift
+//  BookJam
+//
+//  Created by 장준모 on 12/10/23.
+//
+
+import UIKit
+
+class MainDetailActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
