d61fbee49931e64e29e710af5e848a717eeb4864
FpRaArNkK <mspark1184@naver.com>
2023-12-20 00:27:38 +0900

    Feat: LocationViewController UI 이식

--- BookJam/Source/LocationPage/Controller/LocationViewController.swift
+++ BookJam/Source/LocationPage/Controller/LocationViewController.swift
@@ -33,7 +33,33 @@ final class LocationViewController: UIViewController {
     /// 지도 뷰
     private let mapView = MKMapView()
     
+    /// 화면 상단 서치바
+    lazy var searchBar: UISearchBar = UISearchBar().then {
+        $0.placeholder = "보고 싶은 장소를 입력해주세요."
+        $0.layer.cornerRadius = 25
+        $0.clipsToBounds = true
+        $0.searchBarStyle = .minimal
+        
+        $0.layer.borderColor = main02?.cgColor
+        $0.layer.borderWidth = 1
+        $0.setSearchFieldBackgroundImage(UIImage(), for: .normal)
+    }
     
+    /// 목록뷰 상단의 탐색 버튼입니다.
+    lazy var currentLocateBtn: UIButton = UIButton().then {
+        
+        var titleAttr = AttributedString.init("현재 위치로 탐색")
+        titleAttr.font = paragraph04
+        
+        $0.configuration = .filled()
+        $0.configuration?.attributedTitle = titleAttr
+        $0.configuration?.baseForegroundColor = .white
+        $0.configuration?.baseBackgroundColor = main03
+        $0.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        
+        $0.layer.cornerRadius = 15
+        $0.clipsToBounds = true
+    }
     
     // MARK: LifeCycle
     override func viewDidLoad() {
@@ -47,20 +73,42 @@ final class LocationViewController: UIViewController {
 
     // MARK: Configure View
     func setUpView() {
+        // 현재 위치 설정
+        locationManager.requestWhenInUseAuthorization()  // 권한 확인
+        locationManager.startUpdatingLocation() // 위치 업데이트
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest   // 가장 높은 정확도의 위치 정보를 요청
         
+        // 지도 초기화 설정
+        mapView.showsUserLocation = true    // 유저 현재 위치 보이게
+        mapView.mapType = MKMapType.standard    // 일반적인 지도 스타일
+        mapView.setUserTrackingMode(.follow, animated: true)    // 지도가 사용자의 위치를 따라가는 모드로 전환
     }
     
     
     // MARK: Layout
     func setUpLayout() {
         [
-            
-        ].forEach { view.addSubview($0) }
+            mapView,
+            currentLocateBtn,
+            searchBar
+        ].forEach { self.view.addSubview($0)}
     }
     
     
     // MARK: Constraint
     func setUpConstraint() {
+        // 상단 서치바
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(45)
+        }
         
+        // 지도 뷰
+        mapView.snp.makeConstraints {
+            $0.top.equalTo(self.searchBar.snp.bottom).offset(20)
+            $0.left.right.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
 }
