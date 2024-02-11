9dc0d2f313f513edebc4eeaec731a3c86453e6c5
FpRaArNkK <mspark1184@naver.com>
2024-02-12 00:33:14 +0900

    Feat: MyPageHeaderView 추가

--- BookJam/Source/MyPage/Main/View/MyPageHeaderView.swift
+++ BookJam/Source/MyPage/Main/View/MyPageHeaderView.swift
@@ -5,4 +5,57 @@
 //  Created by 박민서 on 2/11/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class MyPageHeaderView: UICollectionReusableView {
+    
+    // MARK: Variables
+    static let id = "MyPageHeaderView"
+    
+    /// 타이틀 라벨
+    private lazy var titleLabel: UILabel = UILabel().then {
+        $0.font = paragraph01
+        $0.textColor = .black
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpView() {
+        self.backgroundColor = .white
+    }
+    
+    private func setUpLayout() {
+        [
+            titleLabel
+        ].forEach { self.addSubview($0) }
+    }
+    
+    private func setUpConstraint() {
+        titleLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(20)
+            $0.right.equalToSuperview().offset(-20).priority(.low)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func configure(title:String) {
+        self.titleLabel.text = title
+    }
+    
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    SearchPageCollecionHeaderView()
+}
