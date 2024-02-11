a3aa55fe1bfd843a985f759272b8b099833ed89b
FpRaArNkK <mspark1184@naver.com>
2024-02-11 22:22:03 +0900

    Feat: MyPageListCellType title 변수 추가

--- BookJam/Source/MyPage/Main/Model/MyPageListCellType.swift
+++ BookJam/Source/MyPage/Main/Model/MyPageListCellType.swift
@@ -30,3 +30,28 @@ enum MyPageListCellType {
     /// 회원탈퇴
     case accountTermination
 }
+
+extension MyPageListCellType {
+    /// 해당 리스트 아이템의 타이틀명
+    var title: String {
+        switch self {
+            
+        case .participatedActivityStatus:
+            return "활동 참여 현황"
+        case .markedActivities:
+            return "찜한 활동"
+        case .changeProfileOrNickname:
+            return "프로필 / 닉네임 변경"
+        case .changePassword:
+            return "비밀번호 변경"
+        case .changeNotificationSetting:
+            return "알림 설정"
+        case .userInquiry:
+            return "회원 문의"
+        case .logout:
+            return "로그아웃"
+        case .accountTermination:
+            return "회원 탈퇴"
+        }
+    }
+}
