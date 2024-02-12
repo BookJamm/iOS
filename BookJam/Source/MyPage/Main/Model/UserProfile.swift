6d3e55d85739fdc08b38b5786eea556381df85e4
FpRaArNkK <mspark1184@naver.com>
2024-02-12 17:35:55 +0900

    Feat: UserProfile Model 추가

---
+++ BookJam/Source/MyPage/Main/Model/UserProfile.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfile.swift
+//  BookJam
+//
+//  Created by 박민서 on 2/12/24.
+//
+
+/// 유저 프로필 표시용 모델
+/// - Front 구현용 모델입니다
+/// - API 연결 시 수정 필
+struct UserProfile {
+    let userId: Int
+    let image: String?
+    let name: String
+}
