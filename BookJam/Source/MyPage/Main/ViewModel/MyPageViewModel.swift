9ee758cede7eb06fc0fb6924947b978601032845
FpRaArNkK <mspark1184@naver.com>
2024-02-12 18:55:31 +0900

    Feat: MyPageViewModel 작성

--- BookJam/Source/MyPage/Main/ViewModel/MyPageViewModel.swift
+++ BookJam/Source/MyPage/Main/ViewModel/MyPageViewModel.swift
@@ -5,4 +5,59 @@
 //  Created by 박민서 on 2/11/24.
 //
 
-import Foundation
+import RxSwift
+import RxRelay
+
+final class MyPageViewModel: ViewModelType {
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: Input
+    struct Input {
+        /// 알림 설정
+        let setNotificationSetting: Observable<Bool>
+    }
+    
+    // MARK: Output
+    struct Output {
+        /// 프로필
+        let profile: Observable<UserProfile>
+    }
+    
+    // MARK: Status
+    /// 알림 설정 상태
+    private let notificationSetting = BehaviorRelay<Bool>(value: false)
+    
+    // MARK: Transform
+    func transform(input: Input) -> Output {
+        
+        input.setNotificationSetting
+            .bind(onNext: { [weak self] val in
+                self?.postNotificationSetting(val: val)
+                    .bind(onNext: { result in
+                        self?.notificationSetting.accept(result)
+                    })
+                    .disposed(by: self?.disposeBag ?? DisposeBag())
+            })
+            .disposed(by: self.disposeBag)
+        
+        return Output(profile: getMyProfile())
+    }
+    
+    // MARK: API Call
+    private func getMyProfile() -> Observable<UserProfile> {
+        return Observable.just(
+            UserProfile(
+                userId: 0,
+                image: nil,
+                name: "test",
+                userEmail: "asdf@naver.com",
+                notificationSetting: false
+            ))
+    }
+    
+    private func postNotificationSetting(val: Bool) -> Observable<Bool> {
+        // 서버에서 서버알림설정 변경 뒤 response로 변경 값 return 받음
+        return Observable.just(val)
+    }
+}
