2061089d013cf951df26b4c9881775bcdfb43375
FpRaArNkK <mspark1184@naver.com>
2024-02-12 22:45:39 +0900

    Feat: CustomAlertViewController 작성

---
+++ BookJam/Source/Common/Controller/CustomAlertViewController.swift
@@ -0,0 +1,157 @@
+//
+//  AlertViewController.swift
+//  BookJam
+//
+//  Created by 박민서 on 2/12/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+
+/// Custom Alert의 Delegate입니다.
+protocol CustomAlertDelegate {
+    var alertWindowType: AlertWindowType { get } // Alert 창 내용
+    func action()   // confirm button event
+    func exit()     // cancel button event
+}
+
+class CustomAlertViewController: UIViewController {
+    
+    var delegate: CustomAlertDelegate?
+    
+    var disposeBag = DisposeBag()
+    
+    /// 중앙 팝업 창
+    private lazy var alertView: UIView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 12
+//        $0.clipsToBounds = true
+        $0.layer.shadowRadius = 10
+        $0.layer.shadowColor = UIColor.gray08.cgColor
+    }
+    
+    /// 컨텐츠 라벨
+    private lazy var contentLabel: UILabel = UILabel().then {
+        $0.numberOfLines = 2 // 최대 두 줄
+        $0.textAlignment = .center
+    }
+    
+    /// confirm 버튼
+    private lazy var confirmButton: UIButton = UIButton().then {
+        $0.backgroundColor = .gray02
+        $0.layer.cornerRadius = 5
+    }
+    
+    /// cancel 버튼
+    private lazy var cancelButton: UIButton = UIButton().then {
+        $0.backgroundColor = .main01
+        $0.layer.cornerRadius = 5
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        setUpLayout()
+        setUpConstraint()
+    }
+    
+
+    // MARK: View
+    private func setUpView() {
+        self.view.backgroundColor = .gray08
+        self.view.layer.opacity = 0.5
+        configureContent()
+    }
+    
+    
+    // MARK: Layout
+    private func setUpLayout() {
+        [
+            contentLabel,
+            confirmButton,
+            cancelButton
+        ].forEach { alertView.addSubview($0) }
+        
+        [
+            alertView
+        ].forEach { view.addSubview($0) }
+    }
+
+    // MARK: Constraint
+    private func setUpConstraint() {
+        
+        alertView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(330)
+            $0.height.equalTo(185)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(30)
+            $0.leading.equalToSuperview().offset(64)
+            $0.trailing.equalToSuperview().offset(-64)
+        }
+        
+        confirmButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.bottom.equalToSuperview().offset(-20)
+            $0.width.equalTo(140)
+            $0.height.equalTo(51)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalToSuperview().offset(-20)
+            $0.width.equalTo(140)
+            $0.height.equalTo(51)
+        }
+
+    }
+    
+    // MARK: Binding
+    private func setUpBinding() {
+        self.confirmButton.rx.tap
+            .debounce(.milliseconds(250), scheduler: MainScheduler())
+            .subscribe(onNext: { [weak self] _ in
+                self?.confirmButtonTapped()
+            })
+            .disposed(by: disposeBag)
+        
+        self.cancelButton.rx.tap
+            .debounce(.milliseconds(250), scheduler: MainScheduler())
+            .subscribe(onNext: { [weak self] _ in
+                self?.cancelButtonTapped()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // 확인 버튼 이벤트 처리
+    private func confirmButtonTapped() {
+        self.dismiss(animated: true) {
+            self.delegate?.action()
+        }
+    }
+    
+    // 취소 버튼 이벤트 처리
+    private func cancelButtonTapped() {
+        self.dismiss(animated: true) {
+            self.delegate?.exit()
+        }
+    }
+    
+    private func configureContent() {
+        if let alertType = self.delegate?.alertWindowType {
+            self.contentLabel.attributedText = alertType.attributedMainText
+            self.confirmButton.setAttributedTitle(alertType.attributedConfirmButtonText, for: .normal)
+            self.cancelButton.setAttributedTitle(alertType.attributedCancelButtonText, for: .normal)
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    CustomAlertViewController()
+}
